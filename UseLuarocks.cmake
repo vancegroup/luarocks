5cb235d778481bb7b0ec457e8ceb765b37a24f1e
Ryan Pavlik <rpavlik@iastate.edu>
2012-03-14 16:18:25 -0500

    Clean up the build-tree install thing

--- UseLuarocks.cmake
+++ UseLuarocks.cmake
@@ -1,9 +1,8 @@
 get_filename_component(_useluarocks_mod_dir "${CMAKE_CURRENT_LIST_FILE}" PATH)
-message(STATUS "Loaded luarocks in-place: ${_useluarocks_mod_dir}")
 
 function(luarocks_install _target _rock _rockstree)
-	file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/luarocks)
 	set(LUAROCKS_BUILDTREE_EXTRAPATH)
+
 	if(TARGET "${LUA_INTERPRETER}")
 		set(LUAROCKS_BUILDTREE_LUA_INTERPRETER "$<TARGET_FILE:${LUA_INTERPRETER}>")
 		set(LUAROCKS_BUILDTREE_LUA_INTERPRETER_DIR "$<TARGET_FILE_DIR:${LUA_INTERPRETER}>")
@@ -15,11 +14,11 @@ function(luarocks_install _target _rock _rockstree)
 
 	if(TARGET ${LUA_LIBRARY})
 		set(LUAROCKS_BUILDTREE_LUA_LIBDIR "$<TARGET_FILE_DIR:${LUA_LIBRARY}>")
-		list(APPEND LUAROCKS_BUILDTREE_EXTRAPATH "$<TARGET_FILE_DIR:${LUA_LIBRARY}>")
 	else()
 		get_filename_component(LUAROCKS_BUILDTREE_LUA_LIBDIR "${LUA_LIBRARY}" PATH)
-		list(APPEND LUAROCKS_BUILDTREE_EXTRAPATH "${LUAROCKS_BUILDTREE_LUA_LIBDIR}")
 	endif()
+	list(APPEND LUAROCKS_BUILDTREE_EXTRAPATH "${LUAROCKS_BUILDTREE_LUA_LIBDIR}")
+
 	set(LUAROCKS_BUILDTREE_SRC_DIR "${_useluarocks_mod_dir}/src")
 
 	if(WIN32)
@@ -36,7 +35,6 @@ function(luarocks_install _target _rock _rockstree)
 	add_custom_command(OUTPUT "${STAMPFILE}"
 		COMMAND
 			"${CMAKE_COMMAND}"
-			"--debug-output"
 			"-DLUA_INTERPRETER:FILEPATH=${LUAROCKS_BUILDTREE_LUA_INTERPRETER}"
 			"-DLUAROCKS_SRC_DIR:PATH=${LUAROCKS_BUILDTREE_SRC_DIR}"
 			"-DEXTRAPATH:STRING=${LUAROCKS_BUILDTREE_EXTRAPATH}"
